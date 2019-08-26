2e58cb063c4f1820710e91d58a741e82cd66c490
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2019-08-26 14:33:44 +0200

    partly responsive a little more

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,18 +9,18 @@ const Contact = () => {
             <Head title="Kontakt"/>
              <div className="form">           
             <h2>Wyślij wiadomość</h2>
-                <form id="contact_form" action="" method="POST" >
+                <form id="contact_form" className="form" action="" method="POST" >
                     <div className="">
                         <label for="name">Twoje imię:</label><br />
-                        <input id="name" className="input" name="name" type="text" value="" size="100" /><br />
+                        <input id="name" className="input" name="name" type="text" value="" size="50" /><br />
                     </div>
                     <div className="">
                         <label for="email">Twój email:</label><br />
-                         <input id="email" className="input" name="email" type="text" value="" size="100" /><br />
+                         <input id="email" className="input" name="email" type="text" value="" size="50" /><br />
                     </div>
                     <div className="">
                         <label for="message">Twoja wiadomość:</label><br />
-                        <textarea id="message" class="input" name="message" rows="7" cols="100"></textarea><br />
+                        <textarea id="message" class="input" name="message" rows="7" cols="50"></textarea><br />
                      </div>
                      <div>
                         <input id="submit_button" type="submit" value="Wyślij email" />
