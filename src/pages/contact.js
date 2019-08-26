12a0f4b2388033ee5c7f3711701f8453ec1334ab
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2019-08-26 15:49:40 +0200

    test1 with resp form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,18 +10,18 @@ const Contact = () => {
              <div className="form-container">           
             <h2>Wyślij wiadomość</h2>
                 <form id="contact_form"  action="" method="POST" >
-                    <div className="form-item">
+                    
                         <label for="name">Twoje imię:</label><br />
                         <input id="name" className="input" name="name" type="text" value="" size="50" /><br />
-                    </div>
-                    <div className="form-item">
+                    
+                    
                         <label for="email">Twój email:</label><br />
                          <input id="email" className="input" name="email" type="text" value="" size="50" /><br />
-                    </div>
-                    <div className="form-item">
+                    
+                   
                         <label for="message">Twoja wiadomość:</label><br />
                         <textarea id="message" className="input" name="message" rows="7" cols="50"></textarea><br />
-                     </div>
+                    
                      <div>
                         <input id="submit_button" type="submit" value="Wyślij email" />
                      </div>
